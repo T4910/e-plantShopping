3dcabecfcc633c1781e771702098b2d10851e85e
Emmanuel Taiwo <ttaiwo4910@gmail.com>
2024-09-29 20:50:11 +0100

    m

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",  // For local preview, set base to "/"
+  base: "/shoppingreact",
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: './index.html',  // Explicitly set the entry point
-    },
-  },
-  root: '.',  // Ensure Vite looks in the root for index.html
-  optimizeDeps: {
-    include: ['react', 'react-dom', '@reduxjs/toolkit', 'react-redux'],
-  },
 })
-y
