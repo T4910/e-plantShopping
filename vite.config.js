b9af8e042c556bb586d09a693a0f65939a9af309
Emmanuel Taiwo <ttaiwo4910@gmail.com>
2024-09-29 04:16:21 +0100

    err

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/",  // For local preview, set base to "/"
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: './index.html',  // Explicitly set the entry point
+    },
+  },
+  root: '.',  // Ensure Vite looks in the root for index.html
+  optimizeDeps: {
+    include: ['react', 'react-dom', '@reduxjs/toolkit', 'react-redux'],
+  },
 })
+y
